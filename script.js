3404df0af18fc52a148cfd0f4890f1d959b05ffd
ayesha-4238 <ayesha05983@gmail.com>
2025-04-07 20:09:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+let display = document.getElementById("display");
+
+function append(char) {
+  if (display.innerText === "0" && char !== ".") {
+    display.innerText = char;
+  } else {
+    display.innerText += char;
+  }
+}
+
+function clearDisplay() {
+  display.innerText = "0";
+}
+
+function deleteLast() {
+  display.innerText = display.innerText.slice(0, -1) || "0";
+}
+
+function calculate() {
+  try {
+    display.innerText = eval(display.innerText.replace('%', '/100'));
+  } catch {
+    display.innerText = "Error";
+  }
+}
